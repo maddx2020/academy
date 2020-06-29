86e1d97fb0027be4ad037613381349a79f4ebf88
Ivan Smirnov <krugvik@mail.ru>
2020-06-29 19:27:30 +0300

    class work 2

---
+++ academy/academy/src/by/academy/classwork/Main.java
@@ -0,0 +1,14 @@
+package by.academy.classwork;
+
+public class Main {
+	public static void main(String... args) {
+		int myAge = 24;
+		long passportNumber = 1234567890L;
+		char yourMark = 'A';
+		double averagemark = 4.12;
+		short test = 512;
+		System.out.println("Your exam resuts:");
+		System.out.println("Your age is: " + myAge + ", passport number: " + passportNumber);
+		System.out.println("You got: " + yourMark + ", average mark is " + averagemark);
+	}
+}
