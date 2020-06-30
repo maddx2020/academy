18083662083d753c17ca855e2967d5700ff61681
maddx2020 <madd.xx83@gmail.com>
2020-06-30 21:08:10 +0300

    последние задания

---
+++ HomeWorkAcademy/src/Lesson2_3.java
@@ -0,0 +1,14 @@
+public class Lesson2_3 {
+
+	public static void main(String[] args) {
+
+		long a = 2_123_000l;
+		long b = 100l;
+		long c = 234l;
+		long z;
+		z = a / b * c;
+		System.out.println((long)z);
+
+	}
+
+}
---
+++ HomeWorkAcademy/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+	public static void main(String... args) {
+		int myAge = 24;
+		long passportNumber = 1234567890L;
+		char yourMark = 'A';
+		double averagemark = 4.12;
+		short test = 512;
+		System.out.println("Your exam resuts:");
+		System.out.println("Your age is: " + myAge + ", passport number: " + passportNumber);
+		System.out.println("You gotttgggggggggt: " + yourMark + ", average mark is " + averagemark);
+	}
+}
