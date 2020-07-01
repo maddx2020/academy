0905dc48b8d8405b18e3b9381cc49f732563009d
maddx2020 <madd.xx83@gmail.com>
2020-07-01 21:19:09 +0300

    from class

---
+++ src/by/academy/Lesson3_3.java
@@ -0,0 +1,19 @@
+package by.academy;
+
+import java.util.Scanner;
+
+public class Lesson3_3 {
+
+	public static void main(String[] args) {
+		int sum = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Введите число: ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			sum *= i;
+		}
+		System.out.println("Произведение чисел равна " + sum);
+
+	}
+
+}
---
+++ src/by/academy/Lesson3_4.java
@@ -0,0 +1,32 @@
+package by.academy;
+
+import java.util.Scanner;
+
+public class Lesson3_4 {
+	int i;
+
+	public static void main(String[] args) {
+		int number = 0;
+		int mcount=0,pcount=0,count=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Введите сколько чисел будете вводить с клавиатуры: ");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.print("Введите " + (i + 1) + " число: ");
+			number = sc.nextInt();
+			if (number>0) {
+				pcount++;
+			}
+			else{
+				if(number<0) {
+					mcount++;
+			}
+				else
+					count++;
+		}
+		
+		}
+		System.out.println("Количество положительных чисел: " + pcount + " Количество отрицательных чисел: " + mcount + " Количество чисел c 0: " + count);
+	}
+	
+}
