5e6412af0ca3d97eaed293b4b0549201b52691f2
maddx2020 <madd.xx83@gmail.com>
2020-07-14 10:54:32 +0300

    ex2 last upd

--- src/by/academy/Lesson5/OOP/Ex2/Person.java
+++ src/by/academy/Lesson5/OOP/Ex2/Person.java
@@ -1,5 +1,59 @@
 package by.academy.Lesson5.OOP.Ex2;
 
-public class Person {
+import java.io.Serializable;
 
+public class Person implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String fullName;
+	private int age;
+
+	public Person() {
+		super();
+	}
+
+	private Person(String fullName, int age) {
+		super();
+		this.fullName = fullName;
+		this.age = age;
+	}
+
+	private String getFullName() {
+		return fullName;
+	}
+
+	private void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	private int getAge() {
+		return age;
+	}
+
+	private void setAge(int age) {
+		this.age = age;
+	}
+
+	void move() {
+		System.out.println("Person " + fullName + " move");
+	}
+
+	void speak() {
+		System.out.println("Person " + fullName + " speak");
+	}
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.fullName = "Bill Colins";
+		person.move();
+		person.speak();
+
+		Person person1 = new Person("Tom Spenser", 29);
+		person1.move();
+		person1.speak();
+
+	}
 }
