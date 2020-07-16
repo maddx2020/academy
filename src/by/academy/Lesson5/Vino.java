e456b4409660db518e1e57e60e77b6878e2a5f58
maddx2020 <madd.xx83@gmail.com>
2020-07-17 01:15:22 +0300

    upd

--- src/by/academy/Lesson5/Vino.java
+++ src/by/academy/Lesson5/Vino.java
@@ -1,25 +1,32 @@
 package by.academy.Lesson5;
 
-public class Vino extends Apple {
+public class Vino extends Product {
+	
 	int discount;
 
 	public Vino(String name, int quantity, double price) {
 		super(name, quantity, price);
 	}
 
-	@Override
 	public int discount() {
-		if (quantity < 3) {
+
+		if (quantity < 5) {
 			discount = 10;
 		} else {
 			if (quantity >= 5 && quantity <= 10) {
-				discount = 18;
+				discount = 15;
 			} else {
-				discount = 25;
+				discount = 20;
 			}
 		}
 		return discount;
 
+	}
+
+	@Override
+	public double fullSum() {
+		discount();
+		return price * quantity * (100 - discount) / 100;
 	}
 
 }
