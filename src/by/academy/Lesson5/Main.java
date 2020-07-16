eda4b742d1fd96544739110dbe7788a4e6aadd9b
maddx2020 <madd.xx83@gmail.com>
2020-07-17 02:25:04 +0300

    files remove

--- src/by/academy/Lesson5/Main.java
+++ src/by/academy/Lesson5/Main.java
@@ -3,14 +3,14 @@ package by.academy.Lesson5;
 public class Main {
 	public static void main(String[] args) {
 
-		User user1 = new User("Вася");
-		User user2 = new User("Леха");
+		User user1 = new User("Р’Р°СЃСЏ");
+		User user2 = new User("Р›РµС…Р°");
 
-		Product prod1 = new Product("хлеб", 1, 1.40);
-		Product prod2 = new Product("молоко", 3, 2.11);
-		Apple prod3 = new Apple("яблоки", 2, 1.50);
+		Product prod1 = new Product("С…Р»РµР±", 1, 1.40);
+		Product prod2 = new Product("РјРѕР»РѕРєРѕ", 3, 2.11);
+		Apple prod3 = new Apple("СЏР±Р»РѕРєРѕ", 2, 1.50);
 		Vino prod4 = new Vino("777", 5, 8);
-		Chees prod5 = new Chees("Дружба", 5, 3);
+		Chees prod5 = new Chees("РґСЂСѓР¶Р±Р°", 5, 3);
 
 		Product[] products = { prod1, prod2, prod3, prod4, prod5 };
 
