8b8851b3ebcf59e9c1b911487d4e6b82eb4ae7de
maddx2020 <madd.xx83@gmail.com>
2020-07-17 14:54:23 +0300

    upd

--- src/by/academy/Lesson5/OOP/Ex11/Main.java
+++ src/by/academy/Lesson5/OOP/Ex11/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Shape circle = new Circle();
-		Shape rectangle = new Rectangle();
-		Shape rect = new Rectangle(0,0,5,5);
+		Circle circle = new Circle();
+		Rectangle rectangle = new Rectangle();
+
+		Rectangle rect = new Rectangle(0,0,5,5);
 
 		Shape[] shapes = { circle, rectangle };
 
