7597f727d8a48165508a87d5041c472954fa996c
maddx2020 <madd.xx83@gmail.com>
2020-07-19 19:30:17 +0300

    upd

--- src/by/academy/Lesson5/OOP/Ex3/Matrix.java
+++ src/by/academy/Lesson5/OOP/Ex3/Matrix.java
@@ -1,10 +1,50 @@
 package by.academy.Lesson5.OOP.Ex3;
 
 public class Matrix {
-	static int a = 2;
-	static int b = 3;
-//vyvod na pechat'
-	public static void printArray(double[][] array) {
+	int a;
+	int b;
+	float[][] array4 = new float[a][b];
+
+	public Matrix() {
+		super();
+	}
+
+	public Matrix(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public float[][] createArray() {
+		getA();
+		getB();
+		float[][] array = new float[a][b];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = (float) Math.random() * 10 + 1;
+			}
+		}
+		return array;
+	}
+
+// vyvod na pechat'
+	public void printArray(float[][] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
 				System.out.print(array[i][j] + " ");
@@ -12,10 +52,10 @@ public class Matrix {
 			System.out.println("");
 		}
 	}
-	
-//unojenie na chislo
-	public static double[][] multiNumber(double[][] array1, double number) {
-		double[][] array3 = new double[a][b];
+
+//umnojenie na chislo
+	public float[][] multiplicationOnNumber(float[][] array1, float number) {
+		float[][] array3 = new float[a][b];
 		for (int i = 0; i < array1.length; i++) {
 			for (int j = 0; j < array1[i].length; j++) {
 				array3[i][j] = array1[i][j] * number;
@@ -23,10 +63,10 @@ public class Matrix {
 		}
 		return array3;
 	}
-	
+
 //summa matric
-	public static double[][] summMatrix(double[][] array1, double[][] array2) {
-		double[][] array3 = new double[a][b];
+	public float[][] summMatrix(float[][] array1, float[][] array2) {
+		float[][] array3 = new float[a][b];
 		for (int i = 0; i < array1.length; i++) {
 			for (int j = 0; j < array1[i].length; j++) {
 				array3[i][j] = array1[i][j] + array2[i][j];
@@ -35,28 +75,4 @@ public class Matrix {
 		return array3;
 	}
 
-	public static void main(String[] args) {
-
-		double[][] array1 = new double[a][b];
-
-		double array2[][] = { { 3.1, 4.2, 5.3 }, { 6.4, 7.5, 8.6 } };
-
-		for (int i = 0; i < array1.length; i++) {
-			for (int j = 0; j < array1[i].length; j++) {
-				array1[i][j] = Math.random() * 10 + 1;
-			}
-		}
-
-		printArray(array1);
-		System.out.println();
-		
-		printArray(array2);
-		System.out.println();
-		
-		printArray(multiNumber(array2, 5));
-		System.out.println();
-		
-		printArray(summMatrix(array2, array2));
-		
-	}
 }
