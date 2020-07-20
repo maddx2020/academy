7b607325beba02712ff187aec34c56ffa25192f9
maddx2020 <madd.xx83@gmail.com>
2020-07-20 19:46:33 +0300

    classw

--- src/by/academy/Lesson7/Reg44.java
+++ src/by/academy/Lesson7/Reg44.java
@@ -7,14 +7,18 @@ public class Reg44 {
 
 	public static void main(String[] args) {
 
-		String[] test = { "50/50/9999", "00/00/000", "12/11/2020", "4/9/1970" };
+		String[] test1 = { "50/50/9999", "00/00/000", "12/11/2020", "4/9/1970" };
+		
+		String[] test2 = { "50-50-9999", "00-00-000", "12-11-2020", "4-9-1970" };
 
 		Pattern p1 = Pattern.compile("([0]?[1-9]|[1|2][0-9]|[3]|[0|1])/([0]?[1-9]|1[0-2])/\\d{4}");
+		
+		Pattern p2 = Pattern.compile("([0]?[1-9]|[1|2][0-9]|[3]|[0|1])-([0]?[1-9]|1[0-2])-\\d{4}");
 
-		for (String t : test) {
-			Matcher m1 = p1.matcher(t);
-			if (m1.find()) {
-				System.out.println(m1.group(0));
+		for (String t : test2) {
+			Matcher m2 = p2.matcher(t);
+			if (m2.find()) {
+				System.out.println(m2.group(0));
 			} else {
 				System.out.println("Неверная дата");
 			}
