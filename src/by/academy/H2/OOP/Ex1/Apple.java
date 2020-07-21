bda026d85cdc44752178bd49d849e64fe592ab85
maddx2020 <madd.xx83@gmail.com>
2020-07-22 02:43:42 +0300

    +

---
+++ src/by/academy/H2/OOP/Ex1/Apple.java
@@ -0,0 +1,18 @@
+package by.academy.H2.OOP.Ex1;
+
+public class Apple extends Phone {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Apple () {
+		super();
+	}
+	
+	protected Apple(int number, String model, double weight) {
+		super(number, model, weight);
+	}
+
+}
---
+++ src/by/academy/H2/OOP/Ex1/Nokia.java
@@ -0,0 +1,17 @@
+package by.academy.H2.OOP.Ex1;
+
+public class Nokia extends Phone {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Nokia() {
+		super();
+	}
+	protected Nokia(int number, String model, double weight) {
+		super(number, model, weight);
+
+	}
+
+}
