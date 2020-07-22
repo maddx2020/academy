49369f6729ab09d76ba34a6139c82ba6fea9acf9
maddx2020 <madd.xx83@gmail.com>
2020-07-22 18:44:56 +0300

    +

---
+++ src/by/academy/Lesson7/Reg55.java
@@ -0,0 +1,10 @@
+package by.academy.Lesson7;
+
+public class Reg55 {
+	public static void main(String[] args) {
+
+		String[] test2 = { "50-50-9999", "00-00-000", "12-11-2020", "4-9-1970" };
+
+	}
+
+}
