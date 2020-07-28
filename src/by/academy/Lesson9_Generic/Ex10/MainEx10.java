96f5b35a8ee1ce581c61db355c94c1a6e804abc6
maddx2020 <madd.xx83@gmail.com>
2020-07-29 00:28:32 +0300

    frome home

--- src/by/academy/Lesson9_Generic/Ex10/MainEx10.java
+++ src/by/academy/Lesson9_Generic/Ex10/MainEx10.java
@@ -5,16 +5,33 @@ public class MainEx10 {
 	public static void main(String[] args) {
 		Integer[] intArr = { 3, 7, 9, 15, 23, 48, 62 };
 		Integer[] intArr2 = { 31, 72, 93, 154, 235, 486, 627 };
+		Double[] doubArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
 
 		Modification<Integer> mod = new Modification<Integer>(intArr);
 		Modification<Integer> mod22 = new Modification<Integer>();
-
+		Modification<Double> doubleMod = new Modification<Double>();
+//task1
 		mod.indexReturnValue(intArr, 6);
-
 		System.out.println();
-				
 		mod22.indexReturnValue(intArr2, 4);
+		System.out.println();
+		doubleMod.indexReturnValue(doubArray, 7);
+		System.out.println();
 
+//task 2
+
+		Integer[] someAr = new Integer[5];
+		someAr[0] = 9;
+		someAr[1] = 8;
+		someAr[2] = 7;
+//		someAr[4] = 2;
+		for (Integer i : someAr) {
+			System.out.print(i + " ");
+		}
+
+	
+	
+	
 	}
 
 }
--- src/by/academy/Lesson9_Generic/Ex10/Modification.java
+++ src/by/academy/Lesson9_Generic/Ex10/Modification.java
@@ -2,7 +2,7 @@
 //	Реализовать метод:
 //	а) который возвращает элемент по индексу (проверка границ массива)
 //	б) который добавляет элемент в конец массива (проверка границ массива)
-//	в) который добавляет элемента в массив по индексу(проверка границ, сдвиг массива)
+//	в) который добавляет элемент в массив по индексу(проверка границ, сдвиг массива)
 //	нужно учесть, что если массив заполнен, увеличиваем массив в 2 раза
 
 package by.academy.Lesson9_Generic.Ex10;
@@ -10,7 +10,7 @@ package by.academy.Lesson9_Generic.Ex10;
 public class Modification<T extends Number> {
 
 	private T[] items;
-	private int index;
+//	private int index;
 	private int result;
 
 	public Modification() {
@@ -30,21 +30,21 @@ public class Modification<T extends Number> {
 		this.items = items;
 	}
 
-	public int getIndex() {
-		return index;
-	}
+//	public int getIndex() {
+//		return index;
+//	}
 
-	public void setIndex(int index) {
-		this.index = index;
-	}
+//	public void setIndex(int index) {
+//		this.index = index;
+//	}
 
-	public int getResult() {
+	private int getResult() {
 		return result;
 	}
 
-	public void setResult(int result) {
-		this.result = result;
-	}
+//	public void setResult(int result) {
+//		this.result = result;
+//	}
 
 	// implement check for array border
 	public void indexReturnValue(T[] items, int index) {
@@ -59,7 +59,10 @@ public class Modification<T extends Number> {
 	}
 
 	// implement check for array border
-	public void addItemToArrayEnd() {
+	public void addItemToEnd(T[] items, double item) {
+		if() {
+			
+		}
 
 	}
 
