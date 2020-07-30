3ef570332c127edd4c4f5b00790fc8f014e5bc8b
maddx2020 <madd.xx83@gmail.com>
2020-07-31 01:24:11 +0300

    +

--- src/by/academy/Lesson7/Reg3.java
+++ src/by/academy/Lesson7/Reg3.java
@@ -4,11 +4,5 @@ public class Reg3 {
 
 	public static void main(String[] args) {
 
-		String str = "Вывести текст, составленный из последних букв\r\n" + 
-				"всех словю";
-		
-		str.
-		
-		
 	}
 }
