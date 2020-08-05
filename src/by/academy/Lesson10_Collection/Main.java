cf7ae978fb37ebd3cbc7a65a0fff965de70b250b
maddx2020 <madd.xx83@gmail.com>
2020-08-05 18:32:43 +0300

    +

---
+++ src/by/academy/Lesson10_Collection/Main.java
@@ -0,0 +1,17 @@
+package by.academy.Lesson10_Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		List<Integer>list = new ArrayList<Integer>();
+//		
+//		List.
+		
+
+		
+	}
+
+}
