eb124c26a1b238653074112cf90c117db092a5dd
maddx2020 <madd.xx83@gmail.com>
2020-09-02 10:41:21 +0300

    +

---
+++ src/by/academy/Lesson13/Task1/Task3.java
@@ -0,0 +1,15 @@
+package by.academy.Lesson13.Task1;
+
+import java.util.function.Predicate;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+	
+		Predicate<String> pr = str->!str.isEmpty();
+		
+		System.out.println(pr.test(""));
+
+	}
+
+}
